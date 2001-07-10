dbb47f9cecb13595241cd7daeb3bc6af27a930e9
Bruce Guenter <bruce@untroubled.org>
2001-07-10 01:38:29 +0000

    Fixed a quoting bug with handling lines starting with ".".

--- protocols/smtp.cc
+++ protocols/smtp.cc
@@ -120,9 +120,8 @@ void smtp::send_data(fdibuf* msg)
   docmd("DATA", 300);
   mystring tmp;
   while(msg->getline(tmp)) {
-    if((tmp[0] == '.' && tmp[1] == 0 && !(out << ".")) ||
-       !(out << tmp << "\r\n"))
-      exit(ERR_MSG_WRITE);
+    if(tmp[0] == '.' && !(out << ".")) exit(ERR_MSG_WRITE);
+    if(!(out << tmp << "\r\n")) exit(ERR_MSG_WRITE);
   }
   docmd(".", 200);
 }
