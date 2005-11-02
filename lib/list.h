a2cfd3adb3650e4fadfda84ec6c4436035a5d889
Bruce Guenter <bruce@untroubled.org>
2005-11-02 16:50:37 +0000

    Fixed compile error in lib/list.h

--- lib/list.h
+++ lib/list.h
@@ -18,8 +18,8 @@ public:
   typedef list_node<T> node;
   typedef list_iterator<T> iter;
   typedef const_list_iterator<T> const_iter;
-  friend class iter;
-  friend class const_iter;
+  friend class list_iterator<T>;
+  friend class const_list_iterator<T>;
   
   list()
     : head(0), tail(0), cnt(0)
