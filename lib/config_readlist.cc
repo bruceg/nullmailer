3f17550ee9ba94dbf5d8582929f84cc5711d6971
Bruce Guenter <bruce@untroubled.org>
2007-10-09 17:33:52 -0600

    Stamped many files with today's date/year.

--- lib/config_readlist.cc
+++ lib/config_readlist.cc
@@ -1,5 +1,5 @@
 // nullmailer -- a simple relay-only MTA
-// Copyright (C) 1999-2003  Bruce Guenter <bruce@untroubled.org>
+// Copyright (C) 2007  Bruce Guenter <bruce@untroubled.org>
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
