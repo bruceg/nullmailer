9663b43742381466855d64112b254a1d095b7a34
Bruce Guenter <bruce@untroubled.org>
2016-01-12 10:26:03 -0600

    lib/defines: Turn defines into real constant strings, not just pointers

--- lib/make_defines.sh
+++ lib/make_defines.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-echo "const char* QUEUE_DIR=\"$1/\";"			>defines.cc
-echo "const char* QUEUE_TMP_DIR=\"$1/tmp/\";"		>>defines.cc
-echo "const char* QUEUE_MSG_DIR=\"$1/queue/\";"		>>defines.cc
-echo "const char* QUEUE_TRIGGER=\"$1/trigger\";"	>>defines.cc
-echo "const char* CONFIG_DIR=\"$2/\";"			>>defines.cc
-echo "const char* PROTOCOL_DIR=\"$3/\";"		>>defines.cc
-echo "const char* BIN_DIR=\"$4/\";"			>>defines.cc
-echo "const char* SBIN_DIR=\"$5/\";"			>>defines.cc
+(
+	echo "extern const char QUEUE_DIR[]=\"$1/\";"
+	echo "extern const char QUEUE_TMP_DIR[]=\"$1/tmp/\";"
+	echo "extern const char QUEUE_MSG_DIR[]=\"$1/queue/\";"
+	echo "extern const char QUEUE_TRIGGER[]=\"$1/trigger\";"
+	echo "extern const char CONFIG_DIR[]=\"$2/\";"
+	echo "extern const char PROTOCOL_DIR[]=\"$3/\";"
+	echo "extern const char BIN_DIR[]=\"$4/\";"
+	echo "extern const char SBIN_DIR[]=\"$5/\";"
+) > defines.cc
