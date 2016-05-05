c973edcd8150dfdbd4ae0451df003a046015f06d
Uffe Jakobsen <uffe@uffe.org>
2016-05-05 11:52:54 +0200

    autogen.sh: test and create phony ChangeLog for automake to succeed

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 aclocal -I .
 autoheader
+# automake needs a file named "ChangeLog"
+if ! test -f "ChangeLog"; then
+echo "INFO: creating phony ChangeLog for automake to succeed";
+touch "ChangeLog";
+fi
 automake --add-missing
 autoconf
