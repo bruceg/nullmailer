ae296433d601cbd41b58386c6417d5db00f019d5
Bruce Guenter <bruce@untroubled.org>
2018-10-11 17:56:34 -0600

    test: Fix smtp-auth test causing cleanup failures

--- test/authtest-smtp.sh
+++ test/authtest-smtp.sh
@@ -1,11 +1,16 @@
 echo 250 ME
+read line
 echo 250-ME
 echo 250-AUTH PLAIN
 echo 250 OK
+read line
 cat $1
 rm -f $1
+read line
 echo 250 OK
+read line
 echo 250 OK
+read line
 echo 351 OK
+read line
 echo 220 OK
-sleep 1
